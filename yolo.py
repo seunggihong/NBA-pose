4719a0b22e04f152c1ffc95f1cac84ddb7b57712
seunggi <hsk1533@naver.com>
2024-05-07 09:57:33 +0900

    add

--- yolo.py
+++ yolo.py
@@ -1,6 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolo/yolov8n-pose.pt')
-result = model(source="video/golf.mp4", conf=.5, save=True, show=True, save_txt=True)
-
-print(result[0].boxes)
+result = model(source="video/test_video.mp4", conf=.3, show=True)
